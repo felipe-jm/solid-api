931aaeefd36ed5295d55df10adb3b3e5a0493204
Felipe Jung <felipemattoseu@gmail.com>
2025-02-09 21:02:10 -0400

    Isolates user creation logic

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { hash } from "bcryptjs";
 import { z } from "zod";
-import { prisma } from "../../lib/prisma";
+import { registerUseCase } from "@/use-cases/register";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const createUserBodySchema = z.object({
@@ -12,27 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = createUserBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userWithSameEmail) {
-    return reply
-      .status(409)
-      .send({ message: "User with same e-mail already exists." });
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (err) {
+    return reply.status(409).send();
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return reply.status(201).send();
 }
