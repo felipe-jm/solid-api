4521e899ccda42f59ca72988b196476d9c21334c
Felipe Jung <felipemattoseu@gmail.com>
2025-02-09 20:55:02 -0400

    Add user registration route

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,7 @@ import { env } from "./env";
 app.listen({ host: "0.0.0.0", port: env.PORT }).then(() => {
   console.log(`HTTP Server Running on http://localhost:${env.PORT}`);
 });
+
+app.get("/", () => {
+  return "Hello World";
+});
