4fa28d7d40da3fde36075e7d2759620f24f9cb21
Felipe Jung <felipemattoseu@gmail.com>
2025-02-10 13:06:49 -0400

    Add register use case unit tests

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/lib/prisma";
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseParams {
   name: string;
@@ -9,17 +9,18 @@ interface RegisterUseCaseParams {
   password: string;
 }
 
-// SOLID
-// Single Responsibility Principle
-// Open/Closed Principle
-// Liskov Substitution Principle
-// Interface Segregation Principle
-// Dependency Inversion Principle
+interface RegisterUseCaseResponse {
+  user: User;
+}
 
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseParams) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseParams): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -28,10 +29,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return { user };
   }
 }
