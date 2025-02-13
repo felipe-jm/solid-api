7c0bbceb50051321b1d4170f4e376bab1e8f5faf
Felipe Jung <felipemattoseu@gmail.com>
2025-02-13 12:53:28 -0400

    Add vitest e2e tests enviroment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import type { Environment } from "vitest";
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "ssr",
+  setup() {
+    console.log("setup");
+
+    return {
+      teardown() {
+        console.log("teardown");
+      },
+    };
+  },
+};
