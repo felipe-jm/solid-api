afba742d2b2296fee2807b73330340a43b071217
Felipe Jung <felipemattoseu@gmail.com>
2025-02-13 20:31:15 -0400

    Add database schema creation for each test suite

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,40 @@
+import "dotenv/config";
+
+import { prisma } from "@/lib/prisma";
+import { randomUUID } from "node:crypto";
 import type { Environment } from "vitest";
+import { execSync } from "node:child_process";
+
+// "postgresql://postgres:docker@localhost:5434/solidapi?schema=public"
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   setup() {
-    console.log("setup");
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync(`npx prisma migrate deploy`);
 
     return {
-      teardown() {
-        console.log("teardown");
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
