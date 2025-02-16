8b559afbce44de98b34af1be0627e4e3071aa679
Felipe Jung <felipemattoseu@gmail.com>
2025-02-16 10:11:11 -0400

    Add authenticate e2e test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,31 @@
-import { expect, test } from "vitest";
+import { it, describe, expect, beforeAll, afterAll } from "vitest";
+import supertest from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {
-  expect(1).toBe(1);
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await supertest(app.server).post("/users").send({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    const response = await supertest(app.server).post("/sessions").send({
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
 });
